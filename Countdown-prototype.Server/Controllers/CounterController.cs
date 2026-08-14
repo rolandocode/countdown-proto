0f9f5d5a94e6ddd8fa1dc06ae712dceccf790ef0
Rolando <rolando.code@gmail.com>
2026-08-14 01:50:54 -0600

    first commit

---
+++ Countdown-prototype.Server/Controllers/CounterController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Countdown_prototype.Server.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CounterController : ControllerBase
+    {
+
+        public CounterController() { }
+
+        [HttpGet]
+        public object Get()
+        {
+            var mexicoTimeZone = TimeZoneInfo.FindSystemTimeZoneById(
+                OperatingSystem.IsWindows()
+                    ? "Central Standard Time"
+                    : "America/Monterrey"
+            );
+
+            var startDate = new DateTime(
+                2026, 8, 10, 18, 0, 0,
+                DateTimeKind.Unspecified
+            );
+
+            var endDate = startDate.AddMonths(9);
+
+            var now = TimeZoneInfo.ConvertTimeFromUtc(
+                DateTime.UtcNow,
+                mexicoTimeZone
+            ).AddHours(-1);
+
+            var totalDuration = endDate - startDate;
+            var elapsed = now - startDate;
+
+            var percentage =
+                elapsed.TotalMilliseconds /
+                totalDuration.TotalMilliseconds * 100;
+
+            percentage = Math.Clamp(percentage, 0, 100);
+
+            return new
+            {
+                startDate = startDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                endDate = endDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                currentDate = now.ToString("yyyy-MM-dd HH:mm:ss"),
+                percentage = percentage
+            };
+        }
+
+        [HttpGet]
+        [Route("day")]
+        public object GetDay()
+        {
+            var mexicoTimeZone = TimeZoneInfo.FindSystemTimeZoneById(
+                OperatingSystem.IsWindows()
+                    ? "Central Standard Time"
+                    : "America/Monterrey"
+            );
+
+            var now = TimeZoneInfo.ConvertTimeFromUtc(
+               DateTime.UtcNow,
+               mexicoTimeZone
+           ).AddHours(-1);
+
+
+            var startDate = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0);
+
+            var endDate = startDate.AddDays(1);
+
+           
+            var totalDuration = endDate - startDate;
+            var elapsed = now - startDate;
+
+            var percentage =
+                elapsed.TotalMilliseconds /
+                totalDuration.TotalMilliseconds * 100;
+
+            percentage = Math.Clamp(percentage, 0, 100);
+
+            return new
+            {
+                startDate = startDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                endDate = endDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                currentDate = now.ToString("yyyy-MM-dd HH:mm:ss"),
+                percentage = percentage
+            };
+        }
+
+        [HttpGet]
+        [Route("work")]
+        public object GetWorkDay()
+        {
+            var mexicoTimeZone = TimeZoneInfo.FindSystemTimeZoneById(
+                OperatingSystem.IsWindows()
+                    ? "Central Standard Time"
+                    : "America/Monterrey"
+            );
+
+            var now = TimeZoneInfo.ConvertTimeFromUtc(
+               DateTime.UtcNow,
+               mexicoTimeZone
+           ).AddHours(-1);
+
+
+            var startDate = new DateTime(now.Year, now.Month, now.Day, 7, 0, 0);
+
+            var endDate = startDate.AddHours(10);
+
+
+            var totalDuration = endDate - startDate;
+            var elapsed = now - startDate;
+
+            var percentage =
+                elapsed.TotalMilliseconds /
+                totalDuration.TotalMilliseconds * 100;
+
+            percentage = Math.Clamp(percentage, 0, 100);
+
+            if (now < startDate || now > endDate && (now.DayOfWeek == DayOfWeek.Saturday || now.DayOfWeek == DayOfWeek.Sunday))
+            {
+                percentage = 0;
+            }
+
+            return new
+            {
+                startDate = startDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                endDate = endDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                currentDate = now.ToString("yyyy-MM-dd HH:mm:ss"),
+                percentage = percentage
+            };
+        }
+    }
+}
---
+++ countdown-prototype.client/src/app/app.ts
@@ -0,0 +1,62 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, OnDestroy, signal } from '@angular/core';
+import { forkJoin } from 'rxjs';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App implements OnInit, OnDestroy {
+  public forecasts: WeatherForecast[] = [];
+  public counterResult: any = {};
+  public counterApiResult: any = {};
+  public workResult: any = {};
+
+  private timerId?: number;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    // Load immediately
+    this.getPercentage();
+
+    // Refresh every 10 seconds
+    this.timerId = window.setInterval(() => {
+      this.getPercentage();
+    }, 10000);
+  }
+
+  getPercentage() {
+    forkJoin({
+      counter: this.http.get('/counter'),
+      counterApi: this.http.get('/counter/day'),
+      work: this.http.get('counter/work')
+    }).subscribe({
+      next: (result) => {
+        this.counterResult = result.counter;
+        this.counterApiResult = result.counterApi;
+        this.workResult = result.work;
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.timerId) {
+      window.clearInterval(this.timerId);
+    }
+  }
+
+  protected readonly title = signal('countdown-prototype.client');
+}
